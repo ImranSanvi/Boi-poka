9d779e284b57afa53cc3cb7d306569dc38b67f2f
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-26 08:42:59 +0600

    book

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,13 +3,13 @@ import bookImg from '../../assets/books.jpg';
 
 const Banner = () => {
     return (
-        <div className='bg-[#131313]/5 flex flex-col md:flex-row justify-between items-center p-[40px] md:p-[100px] my-[20px] md:my-[50px] rounded-2xl mx-4 md:mx-0 space-y-4'>
+        <div className='bg-[#131313]/5 flex flex-col md:flex-row gap-7 justify-between items-center p-[40px] md:p-0 md:pl-[100px] my-[20px] md:my-[50px] rounded-2xl mx-4 md:mx-0 md:h-[400px]'>
             <div className='space-y-4 md:space-y-10'>
                 <h1 className='font-bold text-[56px]'>Books to freshen up your bookshelf</h1>
                 <button className='font-bold text-[20px] text-white py-2 px-4 bg-[#23BE0A] rounded-[10px]'>View The List</button>
             </div>
             <div>
-                <img src={bookImg} alt="" />
+                <img className='md:rounded-r-2xl md:h-[400px]' src={bookImg} alt="" />
             </div>
         </div>
     );
--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -21,14 +21,53 @@ const BookDetails = () => {
   } = singleBook;
 
   return (
-    <div className="flexJ">
-        <img className="w-[200px]" src={image} alt="" />
-        <div>
-            <h3>{bookName}</h3>
-            <h1>By : {author}</h1>
+    <div className="flex flex-col md:flex-row gap-10 m-[30px] md:m-[100px]">
+        <div className=" md:w-1/2 bg-[#131313]/5 flex justify-center items-center">
+          <img className="p-[75px]" src={image} alt="" />
+        </div>
+        <div className="md:w-1/2 space-y-4">
+            <h3 className="font-bold text-[40px]">{bookName}</h3>
+            <h1 className="font-medium text-[20px]">By : {author}</h1>
             <div className="border-t-2 border-gray-300"></div>
-            <p>{category}</p>
+            <p className="font-medium text-[20px] text-[#131313]/80">{category}</p>
             <div className="border-t-2 border-gray-300"></div>
+
+            <p className="font-normal text-[16px] text-[#131313]/70"> <span className="font-bold text-[16px] text-[#131313]">Review:</span> {review}</p>
+
+            <div className="flex gap-10">
+              <span className="font-bold text-[16px] text-[#131313]">Tag</span>
+             {
+                 tags.map(tag => <button className="font-medium text-[16px] text-[#23BE0A] ">{tag}</button>)
+             }
+            </div>
+
+        <div className="border-t-2 border-gray-300"></div>
+
+        <div className='flex gap-15 items-center'>
+          <p className="font-normal text-[16px] ">Number of Pages:</p>
+          <h1 className="font-semibold text-[16px] ">{totalPages}</h1>
+        </div>
+
+        <div className='flex gap-15 items-center'>
+          <p className="font-normal text-[16px] ">Publisher:</p>
+          <h1 className="font-semibold text-[16px] ">{publisher}</h1>
+        </div>
+
+        <div className='flex gap-15 items-center'>
+          <p className="font-normal text-[16px] ">Year of Publishing:</p>
+          <h1 className="font-semibold text-[16px] ">{yearOfPublishing}</h1>
+        </div>
+
+        <div className='flex gap-15 items-center'>
+          <p className="font-normal text-[16px] ">Rating:</p>
+          <h1 className="font-semibold text-[16px] ">{rating}</h1>
+        </div>
+
+
+        <div className="flex flex-col md:flex-row gap-10">
+             <button className="bg-gray-500 px-5 py-3 rounded-2xl">Mark as Read</button>
+            <button className="bg-green-500 px-5 py-3 rounded-2xl">Add to Wishlist</button>
+        </div>
         </div>
     </div>
   );
