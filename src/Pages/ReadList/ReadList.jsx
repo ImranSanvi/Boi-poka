90217277d6d930cbee4e514c34db356fad5fc863
ImranSanvi <imranmohammed5220@gmail.com>
2025-10-26 20:37:26 +0600

    sorting

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -6,20 +6,48 @@ import { getStoredBook } from "../../Utility/AddToDB";
 import ReadBook from "../ReadBook/ReadBook";
 
 const ReadList = () => {
-
   const data = useLoaderData();
 
-  const [readList, setReadList] = useState([])
-  
-  useEffect( () =>{
+  const [readList, setReadList] = useState([]);
+  const [sort , setSort] = useState("");
+
+  useEffect(() => {
     const storedBookData = getStoredBook();
-    const convertedBook = storedBookData.map(id => parseInt(id));
-    const myReadList = data.filter(book => convertedBook.includes(book.bookId));
+    const convertedBook = storedBookData.map((id) => parseInt(id));
+    const myReadList = data.filter((book) =>
+      convertedBook.includes(book.bookId)
+    );
     setReadList(myReadList);
-  },[])
+  }, []);
+
+  const handleSort = (type) => {
+    setSort(type)
+
+    if(type === 'pages'){
+      const sortedByPages = [...readList].sort( (a, b) => a.totalPages - b.totalPages);
+      setReadList(sortedByPages);
+    }
+
+    if(type === 'ratings'){
+      const sortedByRatings = [...readList].sort( (a, b) => a.rating - b.rating);
+      setReadList(sortedByRatings);
+    }
+  }
 
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1 font-bold">Sort By : {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={ () => handleSort("pages")}>Pages</a>
+          </li>
+          <li>
+            <a onClick={ () => handleSort("ratings")}>Rating</a>
+          </li>
+        </ul>
+      </details>
+
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
@@ -27,13 +55,15 @@ const ReadList = () => {
         </TabList>
 
         <TabPanel>
-          <h2 className="text-center mt-[15px] md:mt-[30px]">Book I read {readList.length}</h2>
-          {
-            readList.map(readBook => <ReadBook key={readBook.bookId} readBook={readBook}></ReadBook>)
-          }
+          <h2 className="text-center mt-[15px] md:mt-[30px]">
+            Book I read {readList.length}
+          </h2>
+          {readList.map((readBook) => (
+            <ReadBook key={readBook.bookId} readBook={readBook}></ReadBook>
+          ))}
         </TabPanel>
         <TabPanel>
-          <h2 className="text-center mt-[15px] md:mt-[30px]" >My Wish List</h2>
+          <h2 className="text-center mt-[15px] md:mt-[30px]">My Wish List</h2>
         </TabPanel>
       </Tabs>
     </div>
